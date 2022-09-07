bc035b02eaee394dc76a111f82e090a82fdd2e33
Siwei-Zhou <112421087+Siwei-Zhou@users.noreply.github.com>
2022-09-07 08:11:53 +0800

    Create Medication.py

---
+++ Medication.py
@@ -0,0 +1,8 @@
+"""
+Jack has been prescribed 5 medications, write a variable named "Medication", and store in it the number of medications he has. Print the type of the variable ''Medication". 
+"""
+
+#Store the number of medication
+Medication=5
+#Print the type of the variable "Medication"
+print(type(Medication))
